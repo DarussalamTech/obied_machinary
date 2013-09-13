51305ebcf3218faa05646f6fcc203475d22a4457
ubaid <ubaidullah@darussalampk.com>
2013-09-13 15:56:00 +0500

    Implementation of Backend Product CATEGORIES

---
+++ themes/frontend/views/site/pages/about.php
@@ -0,0 +1,71 @@
+<div class="about_banner">
+    <div class="row">
+        <div class="banner_part">
+            <div class="banner_main_part">
+                <div class="twelve columns">
+                    <img src="<?php echo Yii::app()->theme->baseUrl; ?>/images/inner-banner.png" class="main_banner_img" />
+                </div>
+                <div class="seven columns">
+                    <div class="about_hook">
+                        <img src="<?php echo Yii::app()->theme->baseUrl; ?>/images/truck_hook_03.png" />
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="about_services">
+                        <div class="three columns">
+                            <div class="about_list">
+                                <div class="about_listing">
+                                    All Terrain Cranes
+                                </div>
+                                <div class="about_listing">
+                                    Truck Mounted Cranes
+                                </div>
+                                <div class="about_listing">
+                                    Rough Terrain Cranes
+                                </div>
+                                <div class="about_listing">
+                                    Crawler Cranes
+                                </div>
+                            </div>
+                        </div>
+                        <div class="six columns">
+                            <div class="center_about_service">
+                                <div class="about_upper_part">
+                                    <h2>ABOUT OBEID MACHINERY</h2>
+                                    <p>30 experience-rich years of rock-solid reputation in the industry of heavy machinery and construction equipment. That’s what Obeid Machinery is about.</p>
+                                    <p>We started our operations in 1985 by trading used equipment all over Middle East and Asia.</p>
+                                    <p>Today, in response to the growing demand, we have established Affiliated Offices, such that we cover the globe, to provide a wide range of services and supply specialized equipment to meet the exact requirements of customers within the time frame.</p>
+                                    <section>Our office locations are:</section>
+                                    <article>Houston, U.S.</article>
+                                    <article>Dubai, (U.A.E)</article>
+                                    <article>Seoul, Korea</article>
+                                </div>
+                                <div class="center_about_part">
+                                    <h3>We specializes in trading</h3>
+                                    <article>Mobile cranes</article>
+                                    <article>Other earth moving equipment</article>
+                                    <p>These are available for trading as well as rental purpose. (This will confuse the customers as the machines available for rent are not for sale and vice versa)</p>
+                                    <h4>We hold strong beliefs and work towards making them a reality. Our vision &amp; mission statements clarify our philosophy, which is simple and focused:</h4>
+                                </div>
+                                <div class="lower_about_part">
+                                    <p>We hold strong beliefs and work towards making them a reality.</p>
+                                    <p>Our vision &amp; mission statements clarify our philosophy, which is simple and focused:</p>
+                                    <h4>Vision:</h4>
+                                    <p>TO BE THE TOP PRIORITY FOR THOSE SEEKING HEAVY MACHINERY</p>
+                                    <h4>Mission:</h4>
+                                    <p>TO PROVIDE BEST QUALITY SERVICE, MAXIMUM CUSTOMER SATISFACTION &amp; BUILD LONG-TERM BOND WITH OUR WORTHY CUSTOMERS</p>
+                                    <p>Our aim is to provide you the highest level of service, ensure your maximum satisfaction with our service and we want to build long-term relationship with you. We do not believe in making one-time sales. We believe in converting business deals into relationships.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="three columns">
+                            <div class="right_index_service">
+                                <h4>NEW ARRIVALS</h4>
+                                <div class="ltm_tank">
+                                    <img src="<?php echo Yii::app()->theme->baseUrl; ?>/images/ltm_tank_img_03.jpg" />
+                                    <p><i>LTM1500 with 84m Boom LTM1300</i></p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
