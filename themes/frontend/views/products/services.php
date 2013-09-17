200fad8fc73b5e49fb3a2908b5b90cad42a9dae0
ubaid <ubaidullah@darussalampk.com>
2013-09-17 14:23:29 +0500

    WOrking on controllers

--- themes/frontend/views/products/services.php
+++ themes/frontend/views/products/services.php
@@ -1,32 +1,58 @@
 <div class="row">
     <div class="our_services">
-        <div class="nine columns">
+        <div class="three columns">
+            <div class="about_list">
+                <div class="about_listing">
+                    All Terrain Cranes
+                </div>
+                <div class="about_listing">
+                    Truck Mounted Cranes
+                </div>
+                <div class="about_listing">
+                    Rough Terrain Cranes
+                </div>
+                <div class="about_listing">
+                    Crawler Cranes
+                </div>
+            </div>
+        </div>
+        <div class="six columns">
             <div class="center_about_service">
                 <div class="about_upper_part">
-                    <h2>SERVICES OBEID MACHINERY</h2>
-                    <p>30 experience-rich years of rock-solid reputation in the industry of heavy machinery and construction equipment. That’s what Obeid Machinery is about.</p>
-                    <p>We started our operations in 1985 by trading used equipment all over Middle East and Asia.</p>
-                    <p>Today, in response to the growing demand, we have established Affiliated Offices, such that we cover the globe, to provide a wide range of services and supply specialized equipment to meet the exact requirements of customers within the time frame.</p>
-                    <section>Our office locations are:</section>
-                    <article>Houston, U.S.</article>
-                    <article>Dubai, (U.A.E)</article>
-                    <article>Seoul, Korea</article>
-                </div>
-                <div class="center_about_part">
-                    <h3>We specializes in trading</h3>
-                    <article>Mobile cranes</article>
-                    <article>Other earth moving equipment</article>
-                    <p>These are available for trading as well as rental purpose. (This will confuse the customers as the machines available for rent are not for sale and vice versa)</p>
-                    <h4>We hold strong beliefs and work towards making them a reality. Our vision &amp; mission statements clarify our philosophy, which is simple and focused:</h4>
-                </div>
-                <div class="lower_about_part">
-                    <p>We hold strong beliefs and work towards making them a reality.</p>
-                    <p>Our vision &amp; mission statements clarify our philosophy, which is simple and focused:</p>
-                    <h4>Vision:</h4>
-                    <p>TO BE THE TOP PRIORITY FOR THOSE SEEKING HEAVY MACHINERY</p>
-                    <h4>Mission:</h4>
-                    <p>TO PROVIDE BEST QUALITY SERVICE, MAXIMUM CUSTOMER SATISFACTION &amp; BUILD LONG-TERM BOND WITH OUR WORTHY CUSTOMERS</p>
-                    <p>Our aim is to provide you the highest level of service, ensure your maximum satisfaction with our service and we want to build long-term relationship with you. We do not believe in making one-time sales. We believe in converting business deals into relationships.</p>
+                    <h2>OUR SERVICES</h2>
+                    <p>We never compromise on the quality of our products and services. Customer preference is our top priority, along with maximum customer satisfaction, and relationship-building.</p>
+                    <p>Our services include:</p>
+                    <p>Trading of used construction equipment</p>
+                    <p>Rental facility of any equipment required</p>
+                    <p>On-site maintenance of our machines <span>(please specify that this is only for the rental equipment)</span></p>
+                    <p>On-site check-ups of our machines <span>(please specify that this is only for the rental equipment)</span></p>
+                    <h3>Rental Division</h3>
+                    <p>Our Rental division can provide you any equipment you are looking for, within a short period of time. With our skilled operators and mechanics we can also provide on-site maintenance or check-ups of our machines. </p>
+                    <p>We specialize in the following equipment:</p>
+                    <p>- Cranes ( Kato, Tadano, Sany, Lorain, Terex, Demag)</p>
+                    <p>- Excavators (CAT, Kobelco, Komatsu, Soosan, Hyundai)</p>
+                    <p>- Loaders (JCB, FURUKAWA, KAWASAKi, CAT)</p>
+                    <p>- Forklifts (TCM, ETC)</p>
+                    <h3>Trading Division</h3>
+                    <p>We also have a trading division for construction equipment.</p>
+                    <p>We have been selling used construction machinery for the past 30 Years in The Kingdom Of Saudi Arabia and this is what we love doing.</p>
+                    <p>All our machines are inspected thoroughly on arrival &amp; departure to make sure you get the quality you deserve.</p>
+                    <p>We specialize in the following equipment: </p>
+                    <h4>1)	Cranes</h4>
+                    <li>Rough terrain</li>
+                    <li>Truck Mounted</li>
+                    <li>All-Terrain</li>
+                    <h4>2)	Excavators</h4>
+                    <li>Wheel Excavators</li>
+                    <li>Track Excavators</li>
+                    <li>Mini Excavators </li>
+                    <h4>3)	Loaders </h4>
+                    <li>Wheel Loaders</li>
+                    <li>JCB Loaders</li>
+                    <h4>4)	Road Pavement Equipment</h4>
+                    <h4>5)	Trucks</h4>
+                    <h4>6)	Man Lifts</h4>
+                    <p>And here’s the best part: Apart from the listed equipment we can arrange any equipment that you are looking for, within the time frame you assign us. And we deliver it to wherever you want!</p>
                 </div>
             </div>
         </div>
@@ -34,7 +60,7 @@
             <div class="right_index_service">
                 <h4>NEW ARRIVALS</h4>
                 <div class="ltm_tank">
-                    <img src="<?php echo Yii::app()->theme->baseUrl; ?>/images/ltm_tank_img_03.jpg" />
+                    <img src="<?php echo Yii::app()->theme->baseUrl;?>/images/ltm_tank_img_03.jpg" />
                     <p><i>LTM1500 with 84m Boom LTM1300</i></p>
                 </div>
             </div>
