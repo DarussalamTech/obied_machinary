f99900a5e72bb0a56b42b83d684bec9fab83a844
ubaid <ubaidullah@darussalampk.com>
2013-10-02 17:57:55 +0500

    changeds in migrations

--- protected/views/products/_search.php
+++ protected/views/products/_search.php
@@ -6,75 +6,52 @@
 
 <div class="wide form">
 
-<?php $form=$this->beginWidget('CActiveForm', array(
-	'action'=>Yii::app()->createUrl($this->route),
-	'method'=>'get',
-)); ?>
+    <?php
+    $form = $this->beginWidget('CActiveForm', array(
+        'action' => Yii::app()->createUrl($this->route),
+        'method' => 'get',
+    ));
+    ?>
 
-	<div class="row">
-		<?php echo $form->label($model,'id'); ?>
-		<?php echo $form->textField($model,'id'); ?>
-	</div>
+    <div class="row">
+        <?php echo $form->label($model, 'id'); ?>
+        <?php echo $form->textField($model, 'id'); ?>
+    </div>
 
-	<div class="row">
-		<?php echo $form->label($model,'category_id'); ?>
-		<?php echo $form->textField($model,'category_id'); ?>
-	</div>
+    <div class="row">
+        <?php echo $form->label($model, 'category_id'); ?>
+        <?php echo $form->textField($model, 'category_id'); ?>
+    </div>
 
-	<div class="row">
-		<?php echo $form->label($model,'product_service_type'); ?>
-		<?php echo $form->textField($model,'product_service_type',array('size'=>6,'maxlength'=>6)); ?>
-	</div>
+    <div class="row">
 
-	<div class="row">
-		<?php echo $form->label($model,'product_name'); ?>
-		<?php echo $form->textField($model,'product_name',array('size'=>50,'maxlength'=>50)); ?>
-	</div>
+        <?php echo $form->label($model, 'product_service_type'); ?>
+        <?php
+        echo $form->dropDownList($model, 'product_service_type', array('Sales' => 'Sales', 'Rental' => 'Rental'), array('prompt' => 'Select Type'));
+        ?>
+    </div>
 
-	<div class="row">
-		<?php echo $form->label($model,'product_description'); ?>
-		<?php echo $form->textArea($model,'product_description',array('rows'=>6, 'cols'=>50)); ?>
-	</div>
+    <div class="row">
+        <?php echo $form->label($model, 'product_name'); ?>
+        <?php echo $form->textField($model, 'product_name', array('size' => 50, 'maxlength' => 50)); ?>
+    </div>
 
-	<div class="row">
-		<?php echo $form->label($model,'product_overview'); ?>
-		<?php echo $form->textField($model,'product_overview',array('size'=>60,'maxlength'=>255)); ?>
-	</div>
+    <div class="row">
+        <?php echo $form->label($model, 'product_description'); ?>
+        <?php echo $form->textField($model, 'product_description', array('rows' => 6, 'cols' => 50)); ?>
+    </div>
 
-	<div class="row">
-		<?php echo $form->label($model,'slug'); ?>
-		<?php echo $form->textField($model,'slug',array('size'=>50,'maxlength'=>50)); ?>
-	</div>
+    <div class="row">
+        <?php echo $form->label($model, 'product_overview'); ?>
+        <?php echo $form->textField($model, 'product_overview', array('size' => 60, 'maxlength' => 255)); ?>
+    </div>
 
-	<div class="row">
-		<?php echo $form->label($model,'deleted'); ?>
-		<?php echo $form->textField($model,'deleted'); ?>
-	</div>
 
-	<div class="row">
-		<?php echo $form->label($model,'create_time'); ?>
-		<?php echo $form->textField($model,'create_time'); ?>
-	</div>
 
-	<div class="row">
-		<?php echo $form->label($model,'create_user_id'); ?>
-		<?php echo $form->textField($model,'create_user_id',array('size'=>11,'maxlength'=>11)); ?>
-	</div>
+    <div class="row buttons">
+        <?php echo CHtml::submitButton('Search', array("class" => "btn")); ?>
+    </div>
 
-	<div class="row">
-		<?php echo $form->label($model,'update_time'); ?>
-		<?php echo $form->textField($model,'update_time'); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->label($model,'update_user_id'); ?>
-		<?php echo $form->textField($model,'update_user_id',array('size'=>11,'maxlength'=>11)); ?>
-	</div>
-
-	<div class="row buttons">
-		<?php echo CHtml::submitButton('Search'); ?>
-	</div>
-
-<?php $this->endWidget(); ?>
+    <?php $this->endWidget(); ?>
 
 </div><!-- search-form -->
